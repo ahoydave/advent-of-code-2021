9a25577e40aafede0291382615ea4dc2cfb8e0b9
David Jacka <ahoy.dave@gmail.com>
2021-12-08 20:35:33 +0200

    Add the full day 7

--- day7.clj
+++ day7.clj
@@ -1,4 +1,5 @@
 (ns day7
+  (:require [clojure.edn :as edn])
   (:import (java.lang Math)))
 
 (def example-input [16,1,2,0,4,2,7,1,2,14])
@@ -13,8 +14,8 @@
   ;; Fairly simple to prove it must lie between min/max pos
   ;; Brute force search
   (->> (range (reduce min example-input)
-            (inc (reduce max example-input)))
-     (map (fn [pos] [pos (cost-to-align example-input pos)]))))
+              (inc (reduce max example-input)))
+       (map (fn [pos] [pos (cost-to-align example-input pos)]))))
 
 ;; We can improve the cost calc if we use frequencies
 (defn cost-to-align2 [freqs target]
@@ -30,17 +31,45 @@
 (defn cost-for-pos [position]
   (cost-to-align2 (frequencies example-input) position))
 
-(defn best-position-cost [positions]
+(defn best-position-cost [cost-to-align-fn positions]
   (let [cost-for-pos (fn [position]
-                       (cost-to-align2 (frequencies example-input) position))]
+                       (cost-to-align-fn (frequencies positions) position))]
     (reduce (fn [[best-pos best-cost] new-pos]
               (let [new-cost (cost-for-pos new-pos)]
                 (if (< new-cost best-cost)
                   [new-pos new-cost]
                   [best-pos best-cost])))
-            [(first example-input) (cost-for-pos (first example-input))]
-            (range (apply min example-input)
-                   (inc (apply max example-input))))))
+            [(first positions) (cost-for-pos (first positions))]
+            (range (apply min positions)
+                   (inc (apply max positions))))))
 
 (comment
-  (best-position-cost example-input))
+  (best-position-cost cost-to-align2 example-input)
+
+  (->> (slurp "day7input.txt")
+       (format "[%s]")
+       edn/read-string
+       (best-position-cost cost-to-align2)))
+
+(defn step-increase [n]
+  (/ (* n (+ 1 n)) 2))
+
+(comment
+  (->> [1 2 3 4 5]
+       (map step-increase)))
+
+;; for part 2 - a different cost fn
+(defn cost-to-align3 [freqs target]
+  (reduce (fn [acc [position count]]
+            (+ acc (* count 
+                      (step-increase (Math/abs (- position target))))))
+          0
+          freqs))
+
+(comment
+  (best-position-cost cost-to-align3 example-input)
+
+  (->> (slurp "day7input.txt")
+       (format "[%s]")
+       edn/read-string
+       (best-position-cost cost-to-align3)))
